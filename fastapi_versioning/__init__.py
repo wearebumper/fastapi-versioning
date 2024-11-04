3715a5f38ec4e7d80efdcf6b5e3cc3932d6b86a0
Stephen Hallgren <teevio@gmail.com>
2024-11-04 16:37:05 -0500

    print

--- fastapi_versioning/__init__.py
+++ fastapi_versioning/__init__.py
@@ -1,6 +1,8 @@
 from .routing import versioned_api_route
 from .versioning import VersionedFastAPI, version
 
+print("CUSTOM VERISONISNOIANSFDSOAINDAFS OFOR")
+
 __all__ = [
     "VersionedFastAPI",
     "versioned_api_route",
